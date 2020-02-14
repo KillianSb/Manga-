718f80bc650f6ec06197e87eba756b8ea6fdd194
TheKller29 <TheKller29@Gmail.com>
2020-02-14 10:28:26 +0100

    VBase

--- Manga ++/resources/views/welcome.blade.php
+++ Manga ++/resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Home</title>
+        <title>Manga ++</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -43,7 +43,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Home
+                    Manga ++
                 </div>
 
                 <div class="links">
